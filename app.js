23497b3c5fb54d4b55ca59a138e1e8307351e040
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-07-26 14:33:31 +0200

    game works

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
 let lastHole;
+let timeUp = false;
+let score = 0;
 
 function randomTime(min, max) {
     return Math.round(Math.random() * (max - min) + min);
@@ -23,8 +25,22 @@ function peep() {
     hole.classList.add("up");
     setTimeout(() => {
         hole.classList.remove("up");
-        if(!timeUp) peep();
-
+        if (!timeUp) peep();
     }, time);
-
 };
+
+function startGame() {
+    scoreBoard.textContant = 0;
+    timeUp = false;
+    score = 0;
+    peep();
+    setTimeout(() => timeUp = true, 10000);
+};
+
+function bonk(e) {
+    if (!e.isTrusted) return;
+    score++;
+    this.classList.remove("up");
+    scoreBoard.textContent = score;
+}
+moles.forEach(mole => mole.addEventListener("click", bonk))
